1a731f5cb27c8b302509eab92c6f297ab1d1c855
melih ozguvenc <melihzgvnc@gmail.com>
2026-06-30 15:20:36 +0300

    fix: remove unused line

--- retrieval/web_search.py
+++ retrieval/web_search.py
@@ -3,9 +3,7 @@
 from langchain_tavily import TavilySearch
 
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
-os.environ['TAVILY_API_KEY'] = os.environ.get('TAVILY_API_KEY')
 
 web_search = TavilySearch(max_results=1, include_images=False)
